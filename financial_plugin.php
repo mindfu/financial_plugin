d76d9defb9902eab169153ac61472c7140811543
Chris Scrivo <chris.scrivo@gmail.com>
2014-11-02 17:05:39 -0500

    Added main plugin page

---
+++ financial_plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Financial Plugin
+* Plugin URI: http://mindfu.com/financial_plugin
+* Description: This plugin uses yahoo data to display financial information about companies using shortcodes.
+* Version: 1.0
+* Author: Chris Scrivo
+* Author URI: http://chrisscrivo.com
+* License: GPL12
+*/
+
