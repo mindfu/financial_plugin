761a73112122925381c8fc23b12a01b2a1664237
Chris Scrivo <chris.scrivo@gmail.com>
2014-11-16 12:12:22 -0500

    Clean up code

--- financial_plugin.php
+++ financial_plugin.php
@@ -38,6 +38,7 @@
     * http://www.canbike.org/information-technology/yahoo-finance-url-download-to-a-csv-file.html
      * s l1 d1 t1 c1 o h g v j1 k j e7 r d y 
     */
+
     /* Create a class to get the quote */
     Class yahoo   
     {
@@ -67,76 +68,112 @@
                 $this->eps             = $data[12]; // e7 - EPS Estimate Current Year
                 $this->pe              = $data[13]; // r - P/E Ratio
                 $this->divrate         = $data[14]; // d - Dividend/Share
-                $this->divyield         = $data[15]; // y - Dividend Yield
+                $this->divyield        = $data[15]; // y - Dividend Yield
             }   
         }   
     }   
+        /* Retrieve url of page for refreshing quote every 15 mins */
+        function page_url() 
+        {
+            $page_url = 'http';
+            if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+            $page_url .= "://";
+            if ($_SERVER["SERVER_PORT"] != "80") {
+             $page_url .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+            } else {
+             $page_url .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+            }
+            return $page_url;
+        }
+        
         /* Make the + or - change elicit differing coloration. */
         function get_change_color ($str)   
         {    
             $sign = $str{0};   
-            if ($sign == '+') { $color = 'color:#009900;';   // If we gained print the # in GREEN.
-            } elseif ($sign == '-') { $color =  'color:#ff0000;';   // If we lost RED.
+            if ($sign == '+') { $color = 'color:#009900;';   /* stock gained print # GREEN. */
+            } elseif ($sign == '-') { $color =  'color:#ff0000;';   /* stock lost print RED. */
             } else {  $color = 'font-weight:normal;'; } 
             return $color;
         }
         
-        //register the shortcode with wordpress
-        add_shortcode("financial_plugin_fun", "financial_plugin_handler");
-
-        function financial_plugin_handler($symb) {
+        /* register the shortcode with wordpress */
+        add_shortcode("financial_plugin", "financial_plugin_handler");
+        
+        /* retrieve perameters from shortcode */
+        function financial_plugin_handler($symb) 
+        {
             extract(shortcode_atts(array(
             'symb' => 'symb'
             ), $symb));     
             //run function that actually does the work of the plugin
-            $demolph_output = financialplugin_function($symb);
+            $fin_output = financialplugin_function($symb);
             //send back text to replace shortcode in post
-            return $demolph_output;
+            return $fin_output;
         }
-
-        function financialplugin_function($symb) {
+        
+        /* prepare shortcode output */
+        function financialplugin_function($symb) 
+        {
             // Declare class.   
             $quote = new yahoo; // new stock.
             $symbol = $symb;
-            $quote->get_stock_quote($symbol); // Pass the Company's symbol.   
-
+            $quote->get_stock_quote($symbol); // Pass the Company's symbol.
+        
             if ($quote->last != "0.00") {
+                
+                
+                
+            /*
+            * chart reference codes 
+            * https://code.google.com/p/yahoo-finance-managed/wiki/miscapiImageDownload
+            * http://stackoverflow.com/questions/9807353/getting-stock-graphs-from-yahoo-finance
+            */
+            //$yahoo_chart = "http://ichart.finance.yahoo.com/c/bb/e/".$symbol;
+            $yahoo_chart = "http://ichart.finance.yahoo.com/instrument/1.0/".$symbol."/chart;range=5d/image;size=239x110";
+            
+            $output .= "<div style=\"height:250px;float:left;margin-right:20px;\">";    
 
+                $output .= "<div style=\"width:100%;text-align:center;margin-bottom:10px;\">"; //.$quote->symbol;
+                $output .= "<span style=\"font-size:23px;line-height:30px;\"><strong>\$" .$quote->last. "</strong>&nbsp;&nbsp;"; // price.   
+                $output .= "<span style=\"".get_change_color($quote->change). "\">(".$quote->change.")</span>\n"; // Company symbol 
+                $output .= "</div>";
+                /// CHART 
                 $output .= "<a href=\"http://finance.yahoo.com/q?s=$quote->symbol\" title=\"Yahoo! Finance: " .$symbol.    
-            "\">$quote->symbol</a>\n"; // can use $quote->symbol or $symbol   
-                $output .= "\n";   
-                $output .= "<br>Last Trade: \$" .$quote->last. " (" .$quote->date. " at " .$quote->time. ") \n"; // price.   
-                $output .= '<br>Change: <span style="';   
-                $output .= "" .get_change_color($quote->change). "\">".$quote->change."</span><br>\n";  
-                $output .= "Open: " .$quote->open. "<br>\n";  
-                $output .= "High: " .$quote->high. " Low: " .$quote->low. "<br>\n";  
-                $output .= "Vol: " .$quote->volume. "<br>\n";  
-                $output .= "52 Week High: " .$quote->wkhigh. " 52 Week Low: " .$quote->wklow. "<br>\n";  
-                $output .= "EPS: " .$quote->eps. " PE: " .$quote->pe. "<br>\n";  
-                $output .= "Dividend: " .$quote->divrate. " Yield: " .$quote->divyield. "<br>\n";  
-                $output .= "<br>Mkt Cap: \$" .$quote->capitalization. "\n"; // capitalization.           
-                $output .= "<br>\n";   
-                $output .= "<br>\n"; 
+                "\"><img src=\"$yahoo_chart\"></a><br />";
 
-                /*
-                * CHART
-                * https://code.google.com/p/yahoo-finance-managed/wiki/miscapiImageDownload
-                * http://stackoverflow.com/questions/9807353/getting-stock-graphs-from-yahoo-finance
-                */
-                $yahoo_chart = "http://ichart.finance.yahoo.com/c/bb/e/".$symbol; 
-                $output .= "<a href=\"\"><img src=\"$yahoo_chart\"></a><br /><br />";
-                    //-$yahoo_chart = "http://ichart.finance.yahoo.com/instrument/1.0/".$symbol_lower."/chart;range=1d/image;size=239x110";--
-                $output .= "<div style=\"width:250px;text-align:center;margin-top:0px\">"
-                . "<a href=\"http://finance.yahoo.com/q?s=".$symbol."\" target=\"_blank\">Yahoo Finance</a>"
+                $output .= "<div style=\"width:100%;text-align:center;font-size:10px;\">(" .$quote->date. " at " .$quote->time. ")<br />";
+                $output .= "<a href=\"http://finance.yahoo.com/q?s=".$symbol."\" target=\"_blank\">Yahoo Finance</a>"
                         . "| <a href=\"http://www.google.com/finance?q=".$symbol."\" target=\"_blank\">"
-                        . "Google Finance</a></div>"; 
+                        . "Google Finance</a></div>";   
+            
+            $output .= "</div>";
+            
+            $output .= "<div>"; 
+                $output .= "52 Wk High: " .$quote->wkhigh. "<br>\n";
+                $output .= "52 Wk Low: " .$quote->wklow. "<br>\n";    
+                $output .= "EPS: " .$quote->eps. "<br>\n";
+                $output .= "PE (ttm): " .$quote->pe. "<br>\n"; 
+                $output .= "Div Rate: " .$quote->divrate. "<br>\n";
+                $output .= "Yield: " .$quote->divyield. "<br>\n";  
+                $output .= "Mkt Cap: \$" .$quote->capitalization. "<br>\n";    
+                $output .= "Vol: " .$quote->volume. "<br>\n";   
+                //$output .= "High: " .$quote->high. " Low: " .$quote->low. "<br>\n";
+                //$output .= "Open: " .$quote->open. "<br>\n";          
+                $output .= "<br>\n";   
+            $output .= "<br>\n</div>"; 
+            $output .= "<META http-equiv=\"refresh\" content=\"900;URL=".page_url()."\">"; 
+
+            
+            
 
             } else {
-                //echo "Quote not available.";
+                /* cannot find quote information */
+                echo "Quote not available.";
             }
-            //process plugin
-            //send back text to calling function
-            return $output;
+            
+        /* return text to calling function */
+        return $output;
+        
         }        
     ?>  
 
