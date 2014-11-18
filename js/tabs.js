e2e5781ff39d88447490ae98e1ab7a63a48e7112
Chris Scrivo <chris.scrivo@gmail.com>
2014-11-18 15:56:52 -0500

    add tabs.js

---
+++ js/tabs.js
@@ -0,0 +1,23 @@
+JQuery (document).ready( function($){
+    $("div.tpp_posts").mouseover( function()){
+        var div = $(this);
+        
+       $.post('wp-admin/admin-ajax.php', {
+           action: "tpp_comments",
+           post_id: $(this).find("a").attr("id")
+        }, function (data) {
+            div.append($(data));
+        }
+        );
+        return false;
+        });
+        
+        $("div.top_post").mouseout( function() {
+            
+            $("post").remove();
+            
+        });
+    });
+    <!-- comment -->
+    
+    
